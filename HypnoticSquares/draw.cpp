d44e4180160c49e599edb5b8b83131586419d9ef
刘涛(大数据中心) <liutao3@tech.cig.com.cn>
2020-07-07 18:22:43 +0800

    优化

--- HypnoticSquares/draw.cpp
+++ HypnoticSquares/draw.cpp
@@ -13,6 +13,24 @@ static float windowWidth { gridCnt * gridWidth + 2*thickness };
 
 int randomNumber(int min, int max);
 
+// give a square's position(eg: x=0, y=0, _width=10), randomly return a point from:
+//   (2.5, 2.5), (2.5, 5.0), (2.5, 7.5)
+//   (5.0, 2.5), (5.0, 5.0), (5, 7.5)
+//   (7.5, 2.5), (7.5, 5.0), (7.5, 7.5)
+sf::Vector2<float>* randomMidPoint(float x, float y, float _width) {
+    float minStep { _width / 4 };
+    int randomX { randomNumber(1, 3) };
+    int randomY { randomNumber(1, 3) };
+    sf::Vector2<float>* res { new sf::Vector2<float>(randomX*minStep+x, randomY*minStep+y) };
+    return res;
+}
+
+// get a square's position by its mid point
+sf::Vector2<float>* getPositionFromMidPoint(sf::Vector2<float>* p, float _wdith) {
+    sf::Vector2<float>* res { new sf::Vector2<float>(p->x-(_wdith/2), p->y-(_wdith/2)) };
+    return res;
+}
+
 sf::RectangleShape* drawRect(float _width, float x, float y) {
     auto rect { new sf::RectangleShape };
     rect->setSize(sf::Vector2(_width, _width));
@@ -24,18 +42,28 @@ sf::RectangleShape* drawRect(float _width, float x, float y) {
 }
 
 void drawInnerRects(sf::RectangleShape* r, std::vector<sf::RectangleShape*>& rs) {
+    // position and width of r
     float x { r->getPosition().x };
     float y { r->getPosition().y };
-    float width { r->getSize().x };
+    float _width { r->getSize().x };
 
+    // position and width of the most inner grid
+    float minWidth { 5.f };  // must have a fixed width
+    sf::Vector2<float>* mid { randomMidPoint(x, y, _width) };
+    sf::Vector2<float>* pos { getPositionFromMidPoint(mid, minWidth) };
+    float maxX { pos->x };
+    float maxY { pos->y };
+
+    // random count of total inner grids
     int totalInnerGridCnt { randomNumber(3, 4) };
 
-    float minWidth { 5.f };  // must have a smallest grid inside
-    float widthDecr { (gridWidth-minWidth) / totalInnerGridCnt };  // each step decrease the width of grid
+    // decrease or increase of width and position of those grids
+    float widthDecr { (gridWidth-minWidth) / totalInnerGridCnt };
+    float xIncr { (maxX-x) / totalInnerGridCnt };
+    float yIncr { (maxY-y) / totalInnerGridCnt };
 
-    int offset { randomNumber(8-totalInnerGridCnt, 13-totalInnerGridCnt) };  // for random position
     for (int i {1}; i<=totalInnerGridCnt; ++i) {
-        rs.push_back(drawRect(width-widthDecr*i, x+offset*i, y+offset*i));
+        rs.push_back(drawRect(_width-widthDecr*i, x+xIncr*i, y+yIncr*i));
     }
     rs.back()->setFillColor(sf::Color(123, 234, 111));
 }
